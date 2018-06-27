a815b3bf90d978a52e355f645024b7ea4ebba095
Vonafo <Timetofap123>
2018-06-27 15:44:54 +0300

    almost full landing of spa

---
+++ build/js/main.js
@@ -0,0 +1,2 @@
+function myFunction(){window.pageYOffset>=sticky?navbar.classList.add("sticky"):navbar.classList.remove("sticky")}window.onscroll=function(){myFunction()};var navbar=document.querySelector(".navbar"),sticky=navbar.offsetTop;
+//# sourceMappingURL=../maps/main.js.map
