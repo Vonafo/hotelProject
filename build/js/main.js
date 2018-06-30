db502d044ecf19928f5c7b6329cdf40472c8a9a0
Vonafo <Timetofap123>
2018-06-30 22:58:16 +0300

    aboutus

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,28 @@
-function myFunction(){window.pageYOffset>=sticky?navbar.classList.add("sticky"):navbar.classList.remove("sticky")}window.onscroll=function(){myFunction()};var navbar=document.querySelector(".navbar"),sticky=navbar.offsetTop;$(function(){$("#reservation__check-in").datepicker({altField:"#reservation__check-in",altFormat:" d MM, yy"})}),$(function(){$("#reservation__check-out").datepicker({altField:"#reservation__check-out",altFormat:" d MM, yy"})});
+function myFunction() {
+    window.pageYOffset >= sticky ? navbar.classList.add("sticky") : navbar.classList.remove("sticky")
+}
+
+window.onscroll = function () {
+    myFunction()
+};
+var navbar = document.querySelector(".navbar"), sticky = navbar.offsetTop;
+$(function () {
+    $("#reservation__check-in").datepicker({altField: "#reservation__check-in", altFormat: " d MM, yy"})
+}), $(function () {
+    $("#reservation__check-out").datepicker({altField: "#reservation__check-out", altFormat: " d MM, yy"})
+});
 //# sourceMappingURL=../maps/main.js.map
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 2000);
+}
