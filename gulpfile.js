d5b4c75b59cd74952d548f54ac2a8a90ee6a7437
Vonafo <Timetofap123>
2018-07-16 17:13:02 +0300

    full adaptation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 'use strict';
-
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     prefixer = require('gulp-autoprefixer'),
+    babel = require('gulp-babel'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
@@ -15,28 +15,28 @@ var gulp = require('gulp'),
     reload = browserSync.reload;
 
 var path = {
-    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+    build: {
         html: 'build/',
-        js: 'build/js/',
         css: 'build/css/',
         img: 'build/img/',
-        fonts: 'build/fonts/'
+        fonts: 'build/fonts/',
+        js:"build/js/"
     },
-    src: { //Пути откуда брать исходники
-        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
-        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+    src: {
+        html: 'src/*.html',
         style: 'src/sass/main.scss',
-        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
-        fonts: 'src/fonts/**/*.*'
+        img: 'src/img//*.*',
+        fonts: 'src/fonts//*.*',
+        js: 'src/js//*.js'
     },
-    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
-        html: 'src/**/*.html',
-        js: 'src/js/**/*.js',
-        style: 'src/sass/**/*.scss',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+    watch: {
+        html: 'src//*.html',
+        style: 'src/scss//*.*',
+        img: 'src/img//*.*',
+        fonts: 'src/fonts//*.*',
+        js: 'src/js//*.js'
     },
-    clean: './build' // ?
+    clean: './build'
 };
 
 var config = {
@@ -46,63 +46,71 @@ var config = {
     tunnel: true,
     host: 'localhost',
     port: 3000,
-    logPrefix: "VONAFO HOTEL"
+    logPrefix: "Vonafo Hotel"
 };
 
 gulp.task('html:build', function () {
-    gulp.src(path.src.html) //Выберем файлы по нужному пути
-        .pipe(rigger()) //Прогоним через rigger
-        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
-        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
-});
-
-gulp.task('js:build', function () {
-    gulp.src(path.src.js) //Найдем наш main файл
-        .pipe(rigger()) //Прогоним через rigger
-        .pipe(sourcemaps.init()) //Инициализируем sourcemap
-        .pipe(uglify()) //Сожмем наш js
-        .pipe(sourcemaps.write("../maps")) //Пропишем карты
-        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
-        .pipe(reload({stream: true})); //И перезагрузим сервер
-});
-
-gulp.task('style:build', function () {
-    gulp.src(path.src.style) //Выберем наш main.scss
-        .pipe(sourcemaps.init()) //То же самое что и с js
-        .pipe(sass()) //Скомпилируем
-        .pipe(prefixer()) //Добавим вендорные префиксы
-        .pipe(cssmin()) //Сожмем
-        .pipe(sourcemaps.write('../maps'))
-        .pipe(gulp.dest(path.build.css)) //И в build
+    gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
         .pipe(reload({stream: true}));
 });
-
+gulp.task("fonts:build", function () {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+        .pipe(reload({stream:true}));
+});
+gulp.task('style:build', function () {
+    gulp.src(path.src.style)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
 gulp.task('image:build', function () {
-    gulp.src(path.src.img) //Выберем наши картинки
-        .pipe(imagemin({ //Сожмем их
+    gulp.src(path.src.img)
+        .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()],
             interlaced: true
         }))
-        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(gulp.dest(path.build.img))
         .pipe(reload({stream: true}));
 });
-
-
-gulp.task('fonts:build', function() {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
 });
-
 gulp.task('build', [
     'html:build',
-    'js:build',
     'style:build',
-    'fonts:build',
-    'image:build'
+    'image:build',
+    'js:build',
+    'fonts:build'
 ]);
-
 
 gulp.task('watch', function(){
     watch([path.watch.html], function(event, cb) {
@@ -111,21 +119,22 @@ gulp.task('watch', function(){
     watch([path.watch.style], function(event, cb) {
         gulp.start('style:build');
     });
-    watch([path.watch.js], function(event, cb) {
-        gulp.start('js:build');
-    });
     watch([path.watch.img], function(event, cb) {
         gulp.start('image:build');
     });
     watch([path.watch.fonts], function(event, cb) {
         gulp.start('fonts:build');
     });
+    watch([path.watch.js], function(event, cb){
+        gulp.start('js:build');
+    });
+    watch([path.watch.js], function(event, cb){
+        gulp.start('fonts:build');
+    });
 });
-
 gulp.task('webserver', function () {
     browserSync(config);
 });
-
 gulp.task('clean', function (cb) {
     rimraf(path.clean, cb);
 });
